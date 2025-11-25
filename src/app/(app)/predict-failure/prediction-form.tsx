09e5ab7e378917320495f2067543d0d2fa3a9882
Ibrahim Bagwan <irbagwan9141@gmail.com>
2025-11-25 13:45:44 +0000

    Try fixing this error: `Console Error: ReactDOM.useFormState has been re

--- src/app/(app)/predict-failure/prediction-form.tsx
+++ src/app/(app)/predict-failure/prediction-form.tsx
@@ -1,7 +1,6 @@
 'use client';
 
-import { useEffect } from 'react';
-import { useFormState } from 'react-dom';
+import { useEffect, useActionState } from 'react';
 import { useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { z } from 'zod';
@@ -48,7 +47,7 @@ export function PredictionForm() {
     },
   });
 
-  const [state, formAction] = useFormState(runPrediction, initialState);
+  const [state, formAction] = useActionState(runPrediction, initialState);
 
   useEffect(() => {
     if (state.error) {
